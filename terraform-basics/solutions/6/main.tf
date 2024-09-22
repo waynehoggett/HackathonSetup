6770db6e636c94692760e5b7028e72f6728e3085
Wayne Hoggett <whoggett@auscript.com.au>
2024-09-22 14:07:12 +1000

    Updates

--- terraform-basics/solutions/6/main.tf
+++ terraform-basics/solutions/6/main.tf
@@ -18,7 +18,7 @@ data "azurerm_resource_group" "rg" {
   name = "%RG_NAME%"
 }
 
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "vnet1" {
   name                = "vnet1"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
