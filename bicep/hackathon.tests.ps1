48d192ce74ad90b2d95019fff19f45c0224a4714
Wayne Hoggett <waynehoggett@hotmail.com>
2025-01-19 12:46:41 +1000

    Updates

--- bicep/hackathon.tests.ps1
+++ bicep/hackathon.tests.ps1
@@ -143,7 +143,7 @@ Describe "What If and Deployment Modes" -Tags 4 {
 
 # Challenge 6 - Decorating Parameters, and Interpolation
 
-Describe "Decorating Parameters, and Interpolation" {
+Describe "Decorating Parameters, and Interpolation" -Tags 5 {
     It "main.bicep should contain @minLength(3)" {
         $BicepFile = Get-Content -Path "C:\Bicep\main.bicep"
         $StorageAccountName = $BicepFile | Select-String -Pattern "@minLength(3)" -SimpleMatch
@@ -174,7 +174,7 @@ Describe "Decorating Parameters, and Interpolation" {
 
 # Challenge 7 - Deploying Multiple Resources with Loops and Batching
 
-Describe "Symbolic Names and Outputs" {
+Describe "Symbolic Names and Outputs" -Tags 6 {
     It "" {
 
     }
@@ -182,20 +182,58 @@ Describe "Symbolic Names and Outputs" {
 
 
 # Challenge 8 - Symbolic Names and Outputs
+Describe "Symbolic Names and Outputs" -Tags 7 {
+    It "" {
 
+    }
+}
 
 
 # Challenge 9 - Conditional Deployments
+Describe "Symbolic Names and Outputs" -Tags 8 {
+    It "" {
+
+    }
+}
 
 # Challenge 10 - Working with Existing Resources and Scopes
+Describe "Symbolic Names and Outputs" -Tags 9 {
+    It "" {
+
+    }
+}
 
 # Challenge 11 - Advanced Parameters and Nesting
+Describe "Symbolic Names and Outputs" -Tags 10 {
+    It "" {
 
+    }
+}
 
 # Challenge 12 - Linting and Testing
+Describe "Symbolic Names and Outputs" -Tags 11 {
+    It "" {
+
+    }
+}
 
 # Challenge 13 - Convert Existing Resources
+Describe "Symbolic Names and Outputs" -Tags 12 {
+    It "" {
+
+    }
+}
 
 # Challenge 14 - Modules and Private Registries
+Describe "Symbolic Names and Outputs" -Tags 13 {
+    It "" {
+
+    }
+}
 
 # Challenge 15 - Azure Verified Modules
+Describe "Symbolic Names and Outputs" -Tags 14 {
+    It "" {
+
+    }
+}
